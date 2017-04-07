eb897af2963896922b14354a5ef0b886ff4cf6e3
George <george.e.correa@gmail.com>
2017-04-07 15:46:52 -0400

    Refactored code in App Delegate.

--- NavCtrl/NavControllerAppDelegate.m
+++ NavCtrl/NavControllerAppDelegate.m
@@ -3,7 +3,7 @@
 
 @interface NavControllerAppDelegate ()
 
-//@property (nonatomic, retain) DAO *dataManager;
+@property (nonatomic, retain) DAO *dataManager;
 
 @end
 
@@ -25,22 +25,22 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     [self.window setRootViewController:self.navigationController];
     [self.window makeKeyAndVisible];
-//    self.dataManager = [DAO sharedInstance];
+    self.dataManager = [DAO sharedInstance];
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    DAO *dataManager = [DAO sharedInstance];
-    [dataManager saveCoreData];
+
+    [self.dataManager saveCoreData];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-   DAO *dataManager = [DAO sharedInstance];
-    [dataManager saveCoreData];
+ 
+    [self.dataManager saveCoreData];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -54,13 +54,13 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    DAO *dataManager = [DAO sharedInstance];
-    [dataManager saveCoreData];
+    
+    [self.dataManager saveCoreData];
 }
 
 - (void) dealloc {
     [_navigationController release];
-//    [_dataManager release];
+    [_dataManager release];
     [_window release];
     [super dealloc];
 }
